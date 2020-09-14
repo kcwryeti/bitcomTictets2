2c12078cc065360aa9e1b96eb75a3f63cf75fcd9
Michał Baraniecki <michal@bitcom.com.pl>
2020-09-14 07:06:58 +0200

    wer

--- bitcomTictets/Startup.cs
+++ bitcomTictets/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using OpenIddict.Core;
 
 namespace bitcomTictets
 {
@@ -18,12 +19,26 @@ namespace bitcomTictets
       
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddControllers();
-            services.AddDbContext<ApplicationDbContext>(options =>
-               options.UseSqlServer("Server=DESKTOP-MICHAL\\SQLEXPRESS;Database=bitcomTicets;user=sa;password=Enova123;MultipleActiveResultSets=True"));
+            services.AddDbContext<ApplicationDbContext>(options => {
+                options.UseSqlServer("Server=DESKTOP-MICHAL\\SQLEXPRESS;Database=bitcomTicets;user=sa;password=Enova123;MultipleActiveResultSets=True");
+                options.UseOpenIddict();
+               }) ;
             services.AddIdentity<AppUser, IdentityRole>()
                     .AddEntityFrameworkStores<ApplicationDbContext>()
                     .AddDefaultTokenProviders();
+            services.AddOpenIddict()
+                .AddCore(options =>
+                {
+                    options.UseEntityFrameworkCore()
+                        .UseDbContext<ApplicationDbContext>();
+                }).AddServer(options => { 
+                    
+                    
+                });
+            
+           
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
